a274947de26116380a74108558fb6247c78f59e8
EmilioGonzalez011205 <emiliogonzalez3112@gmail.com>
2025-02-13 13:32:53 -0600

    Main text added

--- lib/Pages/home_page.dart
+++ lib/Pages/home_page.dart
@@ -1,24 +1,66 @@
 import 'package:flutter/material.dart';
 
-class Homepage extends StatefulWidget {
-  const Homepage({super.key});
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
 
   @override
-  State<Homepage> createState() => _HomepageState();
+  State<HomePage> createState() => _HomepageState();
 }
 
-class _HomepageState extends State<Homepage> {
+class _HomepageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.transparent,
-        leading: Icon(
-          Icons.menu,
-          color: Colors.grey[800] ,),
+        leading: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Icon(
+              Icons.menu,
+              color: Colors.grey[800] ,),
+          ),
+        ),
         //Ícono de la izquierda
-      actions: [Icon(Icons.person)],
+      actions: [Padding(
+        padding: const EdgeInsets.only(right: 24.0),
+        child: Icon(Icons.person),
+      )],
       ),
+      body: Column(
+        children: <Widget>[
+        //Texto principal(Text)
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 24.0),
+          child: Row(
+            children: [
+              Text("I want to "),
+              Text("Eat",style:TextStyle(
+                //Tamaño de letra
+                fontSize: 32,
+          
+                //Negritas
+                fontWeight: FontWeight.bold,
+          
+          
+                //Subrayado
+                decoration: TextDecoration.underline
+              ))
+            ],
+          ),
+        )
+
+        //Pestañas(TabBar)
+
+        //Contenido de pestaña (TabBarView)
+
+        //Carrito(Cart)
+
+        ]
+
+
+      )
     );
   }
 }
