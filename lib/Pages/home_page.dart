a506f1713cad1ddf44b1bf8e6f1c459ac4ae1563
EmilioGonzalez011205 <emiliogonzalez3112@gmail.com>
2025-02-17 12:40:46 -0600

    Starting TabBarView

--- lib/Pages/home_page.dart
+++ lib/Pages/home_page.dart
@@ -1,3 +1,9 @@
+import 'package:donut_app_4sa/tabs/burger_tab.dart';
+import 'package:donut_app_4sa/tabs/donut_tab.dart';
+import 'package:donut_app_4sa/tabs/pancakes_tab.dart';
+import 'package:donut_app_4sa/tabs/pizza_tab.dart';
+import 'package:donut_app_4sa/tabs/smoothie_tab.dart';
+import 'package:donut_app_4sa/utils/my_tab.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -8,59 +14,81 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomepageState extends State<HomePage> {
+  List<Widget> myTabs = [
+    const MyTab(iconPath:     "lib/icons/donut.png",),
+    const MyTab(iconPath:     "lib/icons/burger.png",),
+    const MyTab(iconPath:     "lib/icons/smoothie.png",),
+    const MyTab(iconPath:     "lib/icons/pancakes.png",),
+    const MyTab(iconPath:     "lib/icons/pizza.png",),
+  ];
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.transparent,
-        leading: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Padding(
+    return DefaultTabController(
+      length: myTabs.length,
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          leading: Padding(
             padding: const EdgeInsets.all(8.0),
-            child: Icon(
-              Icons.menu,
-              color: Colors.grey[800] ,),
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Icon(
+                Icons.menu,
+                color: Colors.grey[800] ,),
+            ),
           ),
+          //Ícono de la izquierda
+        actions: [Padding(
+          padding: const EdgeInsets.only(right: 24.0),
+          child: Icon(Icons.person),
+        )],
         ),
-        //Ícono de la izquierda
-      actions: [Padding(
-        padding: const EdgeInsets.only(right: 24.0),
-        child: Icon(Icons.person),
-      )],
-      ),
-      body: Column(
-        children: <Widget>[
-        //Texto principal(Text)
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 24.0),
-          child: Row(
-            children: [
-              Text("I want to "),
-              Text("Eat",style:TextStyle(
-                //Tamaño de letra
-                fontSize: 32,
-          
-                //Negritas
-                fontWeight: FontWeight.bold,
-          
-          
-                //Subrayado
-                decoration: TextDecoration.underline
-              ))
-            ],
+        body: Column(
+          children: <Widget>[
+          //Texto principal(Text)
+          const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 24.0),
+            child: Row(
+              children: [
+                Text("I want to "),
+                Text("Eat",style:TextStyle(
+                  //Tamaño de letra
+                  fontSize: 32,
+            
+                  //Negritas
+                  fontWeight: FontWeight.bold,
+            
+            
+                  //Subrayado
+                  decoration: TextDecoration.underline
+                ))
+              ],
+            ),
           ),
+      
+          //Pestañas(TabBar)
+      
+          TabBar(tabs:myTabs),
+      
+          //Contenido de pestaña (TabBarView)
+
+          Expanded(
+            child: TabBarView(children: [
+              DonutTab(),
+              BurgerTab(),
+              SmoothieTab(),
+              PancakesTab(),
+              PizzaTab()
+            ]),
+          )
+      
+          //Carrito(Cart)
+      
+          ]
+      
+      
         )
-
-        //Pestañas(TabBar)
-
-        //Contenido de pestaña (TabBarView)
-
-        //Carrito(Cart)
-
-        ]
-
-
-      )
+      ),
     );
   }
 }
